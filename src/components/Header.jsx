09ae47d08c2c290f2d2cdd8aecd09256643be4c8
leonardo_batista-admin <leonardobatista2003.lb@gmail.com>
2025-11-05 20:58:55 -0300

    v9

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,19 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-gradient-to-r from-[#3A4C60] via-[#1D2D3D] to-[#1D2D3D] text-white shadow-md">
+    <header className="bg-[#1D2D3D] text-white shadow-md">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-2">
-        <div className="flex items-center space-x-3">
-          <div className="p-[3px] rounded-md bg-gradient-to-r from-[#87909F] to-transparent">
+        <div className="flex items-center">
+          <div className="p-[4px] rounded-md bg-white">
             <Image
               src="/bastion_logo.png"
               alt="Bastion Logo"
-              width={80}
-              height={80}
+              width={70}
+              height={70}
               className="h-auto w-auto rounded-sm"
               priority
             />
           </div>
-          <span className="text-lg font-semibold tracking-wide">Bastion</span>
         </div>
         <nav className="hidden md:flex space-x-8 text-sm uppercase">
           <Link href="#home" className="hover:text-[#87909F] transition">Início</Link>
