ab1491c108eba4eeba42cd741c4b8b5b68053480
leonardo_batista-admin <leonardobatista2003.lb@gmail.com>
2025-11-05 20:34:19 -0300

    fix header v4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,20 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full bg-[#2E3E52]/90 backdrop-blur-sm text-white shadow-md z-50 transition-all duration-300">
-      <div className="max-w-7xl mx-auto flex justify-between items-center py-2 px-4">
+    <header className="fixed top-0 left-0 w-full bg-[#33485F]/90 backdrop-blur-sm text-white shadow-md z-50 transition-all duration-300">
+      <div className="max-w-7xl mx-auto flex justify-between items-center py-1.5 px-4">
         <Link href="/" className="flex items-center space-x-2">
           <Image
             src="/bastion_logo.png"
             alt="Bastion Logo"
-            width={130}
+            width={100}   // 🔹 menor
             height={30}
-            className="h-auto w-auto sm:w-[100px] md:w-[140px]"
+            className="h-auto w-auto sm:w-[80px] md:w-[100px]"
             priority
           />
         </Link>
 
-        <nav className="hidden md:flex space-x-6 text-sm font-medium tracking-wide uppercase">
+        <nav className="hidden md:flex space-x-5 text-xs font-medium tracking-wide uppercase">
           <Link href="#home" className="hover:text-[#87909F] transition-colors">
             Início
           </Link>
